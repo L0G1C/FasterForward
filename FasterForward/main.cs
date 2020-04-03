207f8c11d1830379556f7c67b80013768e172eee
Leo Jerez <Leo.Jerez@inmar.com>
2020-04-02 23:06:02 -0400

    Rename

---
+++ FasterForward/main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace FasterForward
+{
+    public class Main : MBSubModuleBase
+    {
+        public override void OnMissionBehaviourInitialize(Mission mission)
+        {
+            var x = Mission.Current.IsCharacterWindowAccessAllowed;  //this is false when entering mission
+
+            // TODO - find a way to intercept TaleWorlds.CampaignSystem.SandBox.Source.TournamentGames.TounramentCampaignBehavior.game_menu_tournament_join_current_game_on_consequence
+        }
+
+    }
+}
